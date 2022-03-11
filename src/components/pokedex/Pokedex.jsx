974ec16b894c790164c478205d5a34bcde2f09c8
IvanMatt8 <ivanmatt1995@gmail.com>
2022-03-11 14:55:57 -0300

    refactor: corrigindo bug

--- src/components/pokedex/Pokedex.jsx
+++ src/components/pokedex/Pokedex.jsx
@@ -1,14 +1,30 @@
 import React from "react";
+import Pagination from "../pagination/Pagination";
 import Pokemon from "../pokemon/Pokemon";
 import "./Pokedex.css";
 
-function Pokedex({ pokemons, loading }) {
-  console.log(pokemons);
+function Pokedex({ pokemons, loading, page, totalPages, setPage }) {
+  const onLeftClickHandler = () => {
+    if (page > 0) {
+      setPage(page - 1);
+    }
+  };
+  const onRightClickHandler = () => {
+    if (page + 1 !== totalPages) {
+      setPage(page + 1);
+    }
+  };
+
   return (
     <div className="pokedexContent">
       <div className="pokedexHeader">
         <h1>Pokedex</h1>
-        <div>Paginação</div>
+        <Pagination
+          page={page + 1}
+          totalPages={totalPages}
+          onLeftClick={onLeftClickHandler}
+          onRightClick={onRightClickHandler}
+        />
       </div>
       {loading ? (
         <div>Carregando, um minuto...</div>
