d5a6498cc4f0ed21e8829f7f5bf486e67de5b469
IvanMatt8 <ivanmatt1995@gmail.com>
2022-03-11 13:11:10 -0300

    feat: acrescentando nome,peso e imagem do pokemon

--- src/components/searchbar/SearchBar.jsx
+++ src/components/searchbar/SearchBar.jsx
@@ -1,15 +1,21 @@
 import React, { useState } from "react";
+import { searchPokemon } from "../../api/api";
 import "./SearchBar.css";
 
-const SearchBar = (props) => {
-  const { onSearch } = props;
+const SearchBar = () => {
   const [search, setSearch] = useState("dito");
+  const [pokemon, setPokemon] = useState();
   const onChangeHandler = (e) => {
     setSearch(e.target.value);
   };
 
   const onButtonClickHandler = () => {
-    console.log("pokemon", search);
+    onSearchHandler(search);
+  };
+
+  const onSearchHandler = async (pokemon) => {
+    const result = await searchPokemon(pokemon);
+    setPokemon(result);
   };
 
   return (
@@ -20,6 +26,13 @@ const SearchBar = (props) => {
       <div className="searchBarButton">
         <button onClick={onButtonClickHandler}>Buscar</button>
       </div>
+      {pokemon ? (
+        <div>
+          <div>Nome:{pokemon.name}</div>
+          <div>Peso:{pokemon.weight}</div>
+          <img src={pokemon.sprites.front_default} alt={pokemon.name} />
+        </div>
+      ) : null}
     </div>
   );
 };
