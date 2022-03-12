98823f5457856723a91065b4eabb54b871b6ae5d
IvanMatt8 <ivanmatt1995@gmail.com>
2022-03-12 09:49:02 -0300

    refactor: encaminhando funcionalidades para o searchContext

--- src/components/pokedex/Pokedex.jsx
+++ src/components/pokedex/Pokedex.jsx
@@ -1,30 +1,17 @@
 import React from "react";
+import { useSearch } from "../../context/searchContext";
 import Pagination from "../pagination/Pagination";
 import Pokemon from "../pokemon/Pokemon";
 import "./Pokedex.css";
 
-function Pokedex({ pokemons, loading, page, totalPages, setPage }) {
-  const onLeftClickHandler = () => {
-    if (page > 0) {
-      setPage(page - 1);
-    }
-  };
-  const onRightClickHandler = () => {
-    if (page + 1 !== totalPages) {
-      setPage(page + 1);
-    }
-  };
+function Pokedex() {
+  const { pokemons, loading, page, totalPages, setPage } = useSearch();
 
   return (
     <div className="pokedexContent">
       <div className="pokedexHeader">
         <h1>Pokedex</h1>
-        <Pagination
-          page={page + 1}
-          totalPages={totalPages}
-          onLeftClick={onLeftClickHandler}
-          onRightClick={onRightClickHandler}
-        />
+        <Pagination />
       </div>
       {loading ? (
         <div>Carregando, um minuto...</div>
