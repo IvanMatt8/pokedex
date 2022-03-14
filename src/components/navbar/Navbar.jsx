0fc34b2f33b6f5396ec1c66767c9ef932b356ebb
IvanMatt8 <ivanmatt1995@gmail.com>
2022-03-14 14:15:25 -0300

    feat: adicionando  imagens da capa

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,18 +1,26 @@
 import React, { useContext } from "react";
 import FavoriteContext from "../../context/favoritesContext";
+import Pokemon from "../../assets/pokemon.png";
+import Charizard from "../../assets/charizard.png";
+import Lugia from "../../assets/lugia.png";
 import "./Navbar.css";
 
 function Navbar() {
   const { favoritePokemons } = useContext(FavoriteContext);
-  const logoImg =
-    "https://raw.githubusercontent.com/PokeAPI/media/master/logo/pokeapi_256.png";
 
   return (
     <nav>
-      <div>
-        <img className="navbarImg" src={logoImg} alt="pokeApi"></img>
+      <div className="iconeCapa">
+        <img src={Pokemon} alt="logoPokemon"></img>
       </div>
-      <div>{favoritePokemons.length}❤️</div>
+      <div>
+        <img
+          className="charizardCapa"
+          src={Charizard}
+          alt="logoCharizard"
+        ></img>
+        <img className="lugiaCapa" src={Lugia} alt="logoLugia"></img>
+      </div>
     </nav>
   );
 }
